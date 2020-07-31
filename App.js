309886262c26d1dea3852617b3c8cf6ba1f8f3d1
Bonny94 <bonazzigiacomo@yahoo.it>
2020-07-31 18:57:26 +0200

    Inizio Login

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, ImageBackground, Button, Alert, TextInput} from 'react-native';
+import * as Font from 'expo-font';
+import {AppLoading} from "expo";
+
+const image = './assets/sunset2.jpg';
+const fetchFonts = () => {
+    return Font.loadAsync({
+        'Caveat': require('./assets/fonts/Caveat-Bold.ttf')
+    });
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>ty6y6 up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [dataLoaded, setDataLoaded] = useState(false);
+
+    if (!dataLoaded) {
+        return (<AppLoading startAsync={fetchFonts} onFinish={() => setDataLoaded(true)}/>);
+    }
+
+    return (
+        <View style={styles.container}>
+            <ImageBackground source={require(image)} style={styles.image}>
+                <Text style={styles.textTitle}>Secret Places</Text>
+                <TextInput
+                    placeholder={"username"}
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                />
+                <TextInput
+                    placeholder={"password"}
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                />
+                <Button
+                    style={styles.login}
+                    title="Login"
+                    onPress={() => Alert.alert('Simple Button pressed')}
+                />
+                <Text> {'Login come ospite'} </Text>
+                <Text> {'Registrazione'} </Text>
+            </ImageBackground>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        flexDirection: "column"
+    },
+    image: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center"
+    },
+    textTitle: {
+        marginTop: "60%",
+        marginLeft: "13%",
+        color: '#ffe0cc',
+        fontSize: 60,
+        fontFamily: 'Caveat',
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    textInput: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1
+    },
+    login: {}
 });
