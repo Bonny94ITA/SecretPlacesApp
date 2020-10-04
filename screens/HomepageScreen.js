b3b4146d4879d5bed48cf9b32caca79f7309554f
Bonny94 <bonazzigiacomo@yahoo.it>
2020-10-04 19:43:27 +0200

    graphic update e buf fix

--- screens/HomepageScreen.js
+++ screens/HomepageScreen.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View,
+    StyleSheet,
+    TouchableWithoutFeedback,
+    Keyboard,
+    ImageBackground} from 'react-native';
 import Header from '../components/Header';
-import {useSelector} from "react-redux";
+
 
 const HomepageScreen = props => {
     return (
         <View style={styles.header}>
             <Header title={"Homepage"}/>
+            <TouchableWithoutFeedback onPress={() => {
+                Keyboard.dismiss();
+            }}>
+                <View style={styles.container}>
+                    <ImageBackground source={require('../assets/sunset.jpg')} style={styles.image}>
+                        <View style={styles.screen}>
+
+                        </View>
+                    </ImageBackground>
+                </View>
+            </TouchableWithoutFeedback>
         </View>
     );
 };
@@ -14,6 +29,27 @@ const HomepageScreen = props => {
 const styles = StyleSheet.create({
     header: {
         flex: 1
+    },
+    screen: {
+        flex: 1,
+        paddingTop: '50%',
+        padding: 10,
+        alignItems: 'center'
+    },
+    container: {
+        flex: 1,
+        flexDirection: "column"
+    },
+    // titleContainer: {
+    //     flex: 0.5,
+    //     flexDirection: 'row',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    // },
+    image: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center"
     }
 });
 
--- screens/NormalSearchScreen.js
+++ screens/NormalSearchScreen.js
@@ -5,96 +5,97 @@ import {
     StyleSheet,
     TouchableWithoutFeedback,
     View,
-    TextInput,
-    Picker
+    Text, ImageBackground
 } from "react-native";
 
-import Colors from "../constants/colors";
+import Colors from '../constants/colors';
 import {Ionicons} from '@expo/vector-icons';
-import Header from "../components/Header";
+import Header from '../components/Header';
 import {Formik} from 'formik';
 import serverURL from '../components/ServerInfo';
-import {useDispatch} from "react-redux";
-import * as authActions from "../store/actions/auth";
-import {setFreeRooms} from "../store/actions/ns";
+import {useDispatch} from 'react-redux';
+import * as authActions from '../store/actions/auth';
+import {setFreeRooms} from '../store/actions/ns';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
 import RNPickerSelect from 'react-native-picker-select';
 
-// function timeout(milliseconds, promise) {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             reject(new Error("Timeout exceeded."))
-//         }, milliseconds);
-//         promise.then(resolve, reject);
-//     });
-// }
+function timeout(milliseconds, promise) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(new Error("Timeout exceeded."))
+        }, milliseconds);
+        promise.then(resolve, reject);
+    });
+}
 
-// async function getCities(dispatch) {
-//     let cities = null;
-//
-//     await timeout(5000, fetch(serverURL + '/hotels/cities'))
-//         .then(async function(response) {
-//             cities = await response.json();
-//             //console.log(cities);
-//         },function(error) {
-//             dispatch(authActions.submitLogout());
-//             console.log(error);
-//         }).catch(function(error) {
-//             dispatch(authActions.submitLogout());
-//             console.log(error);
-//         });
-//
-//     return cities;
-// }
+async function getCities(dispatch) {
+    let cities = null;
 
-// async function normalSearch(city, arrival, departure, dispatch) {
-//     let freeRooms = null;
-//
-//     await timeout(5000, fetch(serverURL + '/hotels/freeRooms', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({
-//             city: city,
-//             arrival: arrival,
-//             departure: departure
-//         })
-//     })).then(async function(response) {
-//         freeRooms = await response.json();
-//     },function(error) {
-//         dispatch(authActions.submitLogout());
-//         console.log(error);
-//     }).catch(function(error) {
-//         dispatch(authActions.submitLogout());
-//         console.log(error);
-//     });
-//
-//     return freeRooms;
-// }
+    await timeout(5000, fetch(serverURL + '/hotels/cities'))
+        .then(async function (response) {
+            cities = await response.json();
+        }, function (error) {
+            dispatch(authActions.submitLogout());
+            console.log(error);
+        }).catch(function (error) {
+            dispatch(authActions.submitLogout());
+            console.log(error);
+        });
+
+    return cities;
+}
+
+async function normalSearch(city, arrival, departure, dispatch) {
+    let freeRooms = null;
+
+    await timeout(5000, fetch(serverURL + '/hotels/freeRooms', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            city: city,
+            arrival: arrival,
+            departure: departure
+        })
+    })).then(async function (response) {
+        freeRooms = await response.json();
+    }, function (error) {
+        dispatch(authActions.submitLogout());
+        console.log(error);
+    }).catch(function (error) {
+        dispatch(authActions.submitLogout());
+        console.log(error);
+    });
+
+    return freeRooms;
+}
 
 const NormalSearchScreen = props => {
     const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
     const [dateArrival, setDateArrival] = useState(new Date(1598051730000));
     const [dateDeparture, setDateDeparture] = useState(new Date(1598051730000));
 
-    // const [selectedValue, setSelectedValue] = useState("Cagliari");
-    // const [pickerItems, setPickerItems] = useState(null);
-    // const dispatch = useDispatch();
+    const [selectedValue, setSelectedValue] = useState("Cagliari");
+    const [pickerItems, setPickerItems] = useState([]);
+    const dispatch = useDispatch();
 
-    // useEffect(() => {
-    //     async function fetchCities(dispatch) {
-    //         const cities = await getCities(dispatch);
-    //
-    //         if (cities !== null) {
-    //             const items = cities.map((s, i) => {
-    //                 //return <Picker.Item key={i} value={s.name} label={s.name}/>
-    //             });
-    //             setPickerItems(items);
-    //         }
-    //     }
-    //     fetchCities(dispatch);
-    // }, []);
+    useEffect(() => {
+
+        async function fetchCities(dispatch) {
+            const cities = await getCities(dispatch);
+
+            if (cities !== null) {
+                const citiesItems = [];
+                const items = cities.map((s, i) => {
+                    citiesItems.push({label: s.name, value: s.name})
+                });
+                setPickerItems(citiesItems);
+            }
+        }
+
+        fetchCities(dispatch);
+    }, []);
 
     const showDatePicker = () => {
         setDatePickerVisibility(true);
@@ -105,13 +106,11 @@ const NormalSearchScreen = props => {
     };
 
     const handleConfirmArrival = (date) => {
-        console.warn("A date has been picked: ", date);
         setDateArrival(date);
         hideDatePicker();
     };
 
     const handleConfirmDeparture = (date) => {
-        console.warn("A date has been picked: ", date);
         setDateDeparture(date);
         hideDatePicker();
     };
@@ -123,75 +122,69 @@ const NormalSearchScreen = props => {
                 Keyboard.dismiss();
             }}>
                 <View style={styles.container}>
-                    <View style={styles.screen}>
-                        <View style={styles.inputContainer}>
-                            <Formik
-                                initialValues={{city: '', arrival: '', departure: ''}}
-                                //onSubmit={async values => {
-                                // const formattedFreeRooms = [];
-                                // const freeRooms = await normalSearch("Cagliari",
-                                //     "12/07/2020", "24/08/2020", dispatch);
-                                // freeRooms.forEach(element => {
-                                //     formattedFreeRooms.push({
-                                //         hotelName: element.hotel.name,
-                                //         hotelStars: element.hotel.stars,
-                                //         hotelAddress: element.hotel.address,
-                                //         idRoom: element.id,
-                                //         numPlaces: element.numPlaces,
-                                //         ppn: element.pricePerNight
-                                //     });
-                                // });
-                                // dispatch(setFreeRooms(formattedFreeRooms));
-                                // props.navigation.navigate('resultsSearch');
-                                //}}
-                            >
-                                {({handleChange, handleBlur, handleSubmit, values}) => (
-                                    <View>
-                                        <Button title="Data di Partenza" onPress={showDatePicker}/>
-                                        <DateTimePickerModal
-                                            isVisible={isDatePickerVisible}
-                                            mode="date"
-                                            onConfirm={handleConfirmArrival}
-                                            onCancel={hideDatePicker}
-                                        />
-                                        <Button title="Data di Arrivo" onPress={showDatePicker}/>
-                                        <DateTimePickerModal
-                                            isVisible={isDatePickerVisible}
-                                            mode="date"
-                                            onConfirm={handleConfirmDeparture}
-                                            onCancel={hideDatePicker}
-                                        />
-                                        {/*<Picker*/}
-                                        {/*    selectedValue={selectedValue}*/}
-                                        {/*    style={{ height: 50, width: 150 }}*/}
-                                        {/*    onValueChange={(itemValue) => setSelectedValue(itemValue)}>*/}
-                                        {/*    {pickerItems}*/}
-                                        {/*</Picker>*/}
-                                        <RNPickerSelect
-                                            onValueChange={(value) => console.log(value)}
-                                            items={[
-                                                { label: 'Football', value: 'football' },
-                                                { label: 'Baseball', value: 'baseball' },
-                                                { label: 'Hockey', value: 'hockey' },
-                                            ]}
-                                        />
-                                        <TextInput
-                                            placeholder={"Città"}
-                                            returnKeyType='next'
-                                            onChangeText={handleChange('city')}
-                                            onBlur={handleBlur('city')}
-                                            style={styles.inputText}
-                                        />
-                                        <Button
-                                            title="Ricerca"
-                                            onPress={handleSubmit}
-                                            color={Colors.primary}
-                                        />
-                                    </View>
-                                )}
-                            </Formik>
+                    <ImageBackground source={require('../assets/sunset2.jpg')} style={styles.image}>
+                        <View style={styles.screen}>
+                            <View style={styles.inputContainer}>
+                                <Formik
+                                    initialValues={{city: '', arrival: '', departure: ''}}
+                                    onSubmit={async values => {
+                                        const formattedFreeRooms = [];
+                                        const freeRooms = await normalSearch("Cagliari",
+                                            "12/07/2020", "24/08/2020", dispatch);
+                                        freeRooms.forEach(element => {
+                                            formattedFreeRooms.push({
+                                                hotelName: element.hotel.name,
+                                                hotelStars: element.hotel.stars,
+                                                hotelAddress: element.hotel.address,
+                                                idRoom: element.id,
+                                                numPlaces: element.numPlaces,
+                                                ppn: element.pricePerNight
+                                            });
+                                        });
+                                        dispatch(setFreeRooms(formattedFreeRooms));
+                                        props.navigation.navigate('resultsSearch');
+                                    }}
+                                >
+                                    {({handleChange, handleBlur, handleSubmit, values}) => (
+                                        <View>
+                                            <Button title="Data di Partenza" onPress={showDatePicker}/>
+                                            <DateTimePickerModal
+                                                isVisible={isDatePickerVisible}
+                                                mode="date"
+                                                onConfirm={handleConfirmArrival}
+                                                onCancel={hideDatePicker}
+                                            />
+                                            <Text> data </Text>
+                                            <Button title="Data di Arrivo" onPress={showDatePicker}/>
+                                            <DateTimePickerModal
+                                                isVisible={isDatePickerVisible}
+                                                mode="date"
+                                                onConfirm={handleConfirmDeparture}
+                                                onCancel={hideDatePicker}
+                                            />
+                                            <Text> data </Text>
+                                            <View style={styles.picker}>
+                                                <RNPickerSelect
+                                                    placeholder={{
+                                                        label: 'Seleziona una città...',
+                                                        value: null,
+                                                    }}
+                                                    selectedValue={selectedValue}
+                                                    onValueChange={(itemValue) => setSelectedValue(itemValue)}
+                                                    items={pickerItems}
+                                                />
+                                            </View>
+                                            <Button
+                                                title="Ricerca"
+                                                onPress={handleSubmit}
+                                                color={Colors.primary}
+                                            />
+                                        </View>
+                                    )}
+                                </Formik>
+                            </View>
                         </View>
-                    </View>
+                    </ImageBackground>
                 </View>
             </TouchableWithoutFeedback>
         </View>
@@ -204,6 +197,7 @@ const styles = StyleSheet.create({
     },
     screen: {
         flex: 1,
+        paddingTop: '50%',
         padding: 10,
         alignItems: 'center'
     },
@@ -220,7 +214,10 @@ const styles = StyleSheet.create({
         padding: 20,
         borderRadius: 10
     },
-    inputText: {
+    picker: {
+        paddingHorizontal: 10,
+        paddingVertical: 8,
+        paddingRight: 30,
         height: 40,
         borderColor: 'orange',
         borderWidth: 1,
@@ -234,16 +231,19 @@ const styles = StyleSheet.create({
         marginVertical: 5,
         width: 225,
     },
+    data: {
+        padding: 10,
+        textAlign: 'center'
+    },
     container: {
         flex: 1,
         flexDirection: "column"
     },
-    titleContainer: {
-        flex: 0.5,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+    image: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center"
+    }
 });
 
 export default NormalSearchScreen;
