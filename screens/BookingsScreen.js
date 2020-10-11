4e6436b3550334cac2470d36fb90c770a4ca6440
Marcellino94 <alberto.guastalla@edu.unito.it>
2020-10-11 18:16:44 +0200

    ptop

--- screens/BookingsScreen.js
+++ screens/BookingsScreen.js
@@ -1,10 +1,60 @@
-import {Button, StyleSheet, Text, View} from "react-native";
-import React from "react";
+import {AsyncStorage, Button, StyleSheet, Text, View} from "react-native";
 import Colors from "../constants/colors";
 import HomepageScreen from "./HomepageScreen";
 import Header from "../components/Header";
+import serverURL from "../components/ServerInfo";
+import * as authActions from "../store/actions/auth";
+import {useDispatch} from "react-redux";
+import React, {useEffect} from 'react';
+
+function timeout(milliseconds, promise) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(new Error("Timeout exceeded."))
+        }, milliseconds);
+        promise.then(resolve, reject);
+    });
+}
+
+async function getBookings(dispatch, token, userId) {
+    let bookings = null;
+
+    console.log(userId);
+    console.log(token)
+    await timeout(5000, fetch(serverURL + '/bookings/id/' + +userId, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            token_info: JSON.stringify({token: token, type: 0})
+        }
+    }))
+        .then(async function (response) {
+            bookings = await response.json();
+        }, function (error) {
+            dispatch(authActions.submitLogout());
+            console.log(error);
+        }).catch(function (error) {
+            dispatch(authActions.submitLogout());
+            console.log(error);
+        });
+
+    return bookings;
+}
 
 const BookingsScreen = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        async function fetchBookings(dispatch) {
+            const userData = await AsyncStorage.getItem('userData');
+            const jsonObj = JSON.parse(userData);
+            const bookings = await getBookings(dispatch, jsonObj.token, 1);
+            console.log(bookings);
+        }
+
+        fetchBookings(dispatch);
+    }, []);
+
     return (
         <View style={styles.header}>
             <Header title={"Prenotazioni"}/>
